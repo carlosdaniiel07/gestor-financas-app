faf22f83f578e261967a8126b53191b9b33f5daa
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2019-10-15 22:43:16 -0300

    Criação tela 'meu perfil'. Issue #7

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { NavController } from '@ionic/angular';
+import { NavController, ModalController } from '@ionic/angular';
+import { MeuPerfilComponent } from './meu-perfil/meu-perfil.component';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,16 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private authService: AuthService, private navController: NavController) {
+  constructor(private authService: AuthService, private navController: NavController, private modalController: ModalController) {
   }
 
   logout(): void {
     this.authService.logout()
+  }
+
+  meuPerfil(): void {
+    this.modalController.create({
+      component: MeuPerfilComponent,
+    }).then((modal) => modal.present())
   }
 }
