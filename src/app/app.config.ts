9fbfa8297c9e29503bbefe504ed30024dc2c804c
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2019-10-26 18:47:32 -0300

    Alterando configurações para build em produção

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
 export const APP_CONFIG  = {
-    //apiUrl: 'https://api-gestor-financas.herokuapp.com'
-    apiUrl: 'http://localhost:8080'
+    apiUrl: 'https://api-gestor-financas.herokuapp.com'
+    //apiUrl: 'http://localhost:8080'
 }
