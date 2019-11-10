a0d25931c22b826836068c241d70a93bad732437
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2019-11-10 18:24:57 -0300

    Ajustando deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e # termina o script com um código diferente de 0 se alguma coisa falhar
 
+echo "[!] Instalando dependências do Node"
+
+npm install
+
 echo "[!] Iniciando build do projeto"
 
 # realiza build do projeto p/ plataforma Android
