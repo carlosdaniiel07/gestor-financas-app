a900889471a6e63caefb06d5136d715025b9c544
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-02-02 17:27:15 -0300

    Ordenar jobs/tasks p/ data de execução (do mais recente para o mais antigo). Issue #41

--- src/app/home/config-page/historico-jobs/historico-jobs.component.ts
+++ src/app/home/config-page/historico-jobs/historico-jobs.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Task } from 'src/app/models/task.model';
+import { Task, sortByDataExecucaoDesc } from 'src/app/models/task.model';
 import { ModalController, AlertController } from '@ionic/angular';
 import { TaskService } from 'src/app/services/task.service';
 import { ToastUtils } from 'src/app/utils/toast.utils';
+import { DateUtils } from 'src/app/utils/date.utils';
 
 @Component({
   selector: 'app-historico-jobs',
@@ -11,11 +12,19 @@ import { ToastUtils } from 'src/app/utils/toast.utils';
 export class HistoricoJobsComponent implements OnInit {
 
   modal: boolean = true
+  showSomenteHoje: boolean = false
+
   @Input() tasks: Task[] = []
+  tasksVisiveis: Task[] = []
 
   constructor(private modalController: ModalController, private alertController: AlertController, private taskService: TaskService, private toast: ToastUtils) { }
 
   ngOnInit() {}
+
+  ionViewWillEnter() {
+   this.tasks = this.tasks.sort(sortByDataExecucaoDesc) 
+   this.tasksVisiveis = this.tasks
+  }
 
   closeModal(): void {
     this.modalController.dismiss()
@@ -32,5 +41,16 @@ export class HistoricoJobsComponent implements OnInit {
         }}
       ]
     }).then((alert) => alert.present())
+  }
+
+  onFiltroChanges(event: any): void {
+    this.showSomenteHoje = !this.showSomenteHoje
+
+    if (this.showSomenteHoje) {
+      let today = DateUtils.toApiPattern(DateUtils.getNowAsJson())
+      this.tasksVisiveis = this.tasks.filter((task: Task) => DateUtils.toApiPattern(task.dataExecucao) == today)
+    } else {
+      this.tasksVisiveis = this.tasks
+    }   
   }
 }
